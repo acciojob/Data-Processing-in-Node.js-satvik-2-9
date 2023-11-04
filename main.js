a488fc96caf826333148eb7d5a51a65e97376e46
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 20:11:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-Implement the processData function in app.js:
+// app.js
+const fs = require('fs').promises;
 
-const fs = require('fs');
-const { Transform } = require('stream');
-
-function processData(inputFilePath, outputFilePath) {
-  // Implement this function
+async function processData(inputFilePath, outputFilePath) {
+  try {
+    const data = await fs.readFile(inputFilePath, 'utf8');
+    const upperCasedData = data.toUpperCase();
+    await fs.writeFile(outputFilePath, upperCasedData);
+  } catch (error) {
+    console.error('Error processing data:', error);
+    process.exit(1); // Exit with error code 1 in case of failure
+  }
 }
 
-processData('input.txt', 'output.txt');
-
+if (require.main === module) {
+  // This will only be true if the script is executed directly from the command line
+  const [inputFilePath, outputFilePath] = process.argv.slice(2);
+  processData(inputFilePath, outputFilePath);
+}
